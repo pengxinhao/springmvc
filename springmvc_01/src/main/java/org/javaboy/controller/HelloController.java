c6c2dd95b405f60a0917b9fe0be9c81201799b6c
pengxinhao <66909864+pengxinhao@users.noreply.github.com>
2020-06-14 22:02:05 +0800

    Add files via upload

---
+++ springmvc_01/src/main/java/org/javaboy/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.javaboy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping(path="/hello")
+    public String sayHello(){
+        System.out.println("Hello StringMvc");
+        return "success";
+    }
+}
---
+++ springmvc_01/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h3>入门程序</h3>
+<a href="/hello">入门程序员</a>
+</body>
+</html>
